79fc86adee53ac34213663226a6cc005e5e70380
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-06-25 18:01:17 -0300

    Add files via upload

---
+++ Lista_04/Primeiro.java
@@ -0,0 +1,44 @@
+public class Primeiro {
+    public static void main(String[] args) {
+        
+        double a = Math.pow(9.0, 3.0);
+        
+        double b = Math.pow(7.0, 5.0);
+        
+        double c1 = Math.pow(3.0, 9.0);
+        double c2 = Math.pow(2.0, 10.0);
+        double ResultadoC = c1+c2;
+        
+        double d1 = Math.pow(5.0, 4.0);
+        double d2 = Math.pow(6.0, 2.0);
+        double ResultadoD = d1-d2;
+        
+        double e1 = Math.pow(5.0, 2.0);
+        double ResultadoE = Math.pow(e1, 3.0);
+        
+        double f1 = Math.pow(2.0, 7.0);
+        double f2 = Math.pow(3.0, 4.0);
+        double f3 = f1-f2;
+        double ResultadoF = Math.cbrt(f3);
+        
+        double g1 = Math.pow(9.0, 7.0);
+        double g2 = Math.pow(4.0, 3.0);
+        double g3 = g1*g2;
+        double ResultadoG = Math.pow(g3, 1/5);
+        
+        double h1 = Math.pow(2.0, 5.0);
+        double h2 = Math.pow(5.0, 5.0);
+        double h3 = h1/h2;
+        double ResultadoH = Math.pow(h3, 1/4);
+        
+        System.out.println("A) " + a);
+        System.out.println("B) " + b);
+        System.out.println("C) " + ResultadoC);
+        System.out.println("D) " + ResultadoD);
+        System.out.println("E) " + ResultadoE);
+        System.out.println("F) " + ResultadoF);
+        System.out.println("G) " + ResultadoG);
+        System.out.println("H) " + ResultadoH);
+    }
+    
+}
---
+++ Lista_04/Segundo.java
@@ -0,0 +1,11 @@
+public class Segundo {
+    public static void main(String[] args) {
+        double log1 = Math.log10(1024);
+        System.out.println("A) " + log1);
+    }
+    
+}
+/*
+double Math.log(double val)
+double Math.log10(double val)
+*/
