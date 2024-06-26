b792f50bb17709eb6b0a593076c93777fc5c960e
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-06-26 14:13:31 -0300

    Add files via upload

---
+++ Lista_03/Primeiro.java
@@ -0,0 +1,9 @@
+public class Primeiro {
+    public static void main(String[] args) {
+        
+        for(int i =1, j=60; j>= 0; i+=3, j-=5){
+            System.out.println("I = "+i + "  J = "+j);
+        }
+    }
+    
+}
