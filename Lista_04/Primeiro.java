920c21ab0831cc61286971ccd61c04975db8ad5d
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-06-26 20:52:32 -0300

    Add files via upload

--- Lista_04/Primeiro.java
+++ Lista_04/Primeiro.java
@@ -2,43 +2,41 @@ public class Primeiro {
     public static void main(String[] args) {
         
         double a = Math.pow(9.0, 3.0);
+        System.out.println("A) " + a);
         
         double b = Math.pow(7.0, 5.0);
+        System.out.println("B) " + b);
         
         double c1 = Math.pow(3.0, 9.0);
         double c2 = Math.pow(2.0, 10.0);
         double ResultadoC = c1+c2;
+        System.out.println("C) " + ResultadoC);
         
         double d1 = Math.pow(5.0, 4.0);
         double d2 = Math.pow(6.0, 2.0);
         double ResultadoD = d1-d2;
+        System.out.println("D) " + ResultadoD);
         
         double e1 = Math.pow(5.0, 2.0);
         double ResultadoE = Math.pow(e1, 3.0);
+        System.out.println("E) " + ResultadoE);
         
         double f1 = Math.pow(2.0, 7.0);
         double f2 = Math.pow(3.0, 4.0);
         double f3 = f1-f2;
         double ResultadoF = Math.cbrt(f3);
+        System.out.println("F) " + ResultadoF);
         
         double g1 = Math.pow(9.0, 7.0);
         double g2 = Math.pow(4.0, 3.0);
         double g3 = g1*g2;
-        double ResultadoG = Math.pow(g3, 1/5);
+        double ResultadoG = Math.pow(g3, 1.0/5);
+        System.out.println("G) " + ResultadoG);
         
         double h1 = Math.pow(2.0, 5.0);
         double h2 = Math.pow(5.0, 5.0);
         double h3 = h1/h2;
-        double ResultadoH = Math.pow(h3, 1/4);
-        
-        System.out.println("A) " + a);
-        System.out.println("B) " + b);
-        System.out.println("C) " + ResultadoC);
-        System.out.println("D) " + ResultadoD);
-        System.out.println("E) " + ResultadoE);
-        System.out.println("F) " + ResultadoF);
-        System.out.println("G) " + ResultadoG);
+        double ResultadoH = Math.pow(h3, 1.0/4);
         System.out.println("H) " + ResultadoH);
-    }
-    
+    }    
 }
---
+++ Lista_04/SegundoC.java
@@ -0,0 +1,19 @@
+public class SegundoC {
+    public static void main(String[] args) {
+        
+       double log_2_1024 = Math.log(1024) / Math.log(2);
+       double log_3_2187 = Math.log(20187) / Math.log(3);
+       double log_3_390625 = Math.log(390625) / Math.log(3);
+       double log_101_1104 = Math.log(1.104622125) / Math.log(1.01);
+       double log_25_244 = Math.log(244.140625) / Math.log(2.5);
+       
+        System.out.println("A) " + log_2_1024);
+        System.out.println("B) " + log_3_2187);
+        System.out.println("C) " + log_3_390625);
+        System.out.println("D) " + log_101_1104 );
+        System.out.println("E) " + log_25_244);
+        
+
+    }
+    
+}
