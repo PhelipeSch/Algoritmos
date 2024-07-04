c33e851a93d978dbd6273b163c64e1764f7f7281
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-07-03 21:46:56 -0300

    Update Exercicio01.java

--- Arrays/Exercicio01.java
+++ Arrays/Exercicio01.java
@@ -1,19 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
-/**
- *
- * @author 05667341093
- */
+import java.util.Scanner;
 public class Exercicio01 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leitor = new Scanner (System.in);
+        
+        String [] nomes = new String [10];
+        double[][] notas = new double [10][2];
+        
+        for(int i = 0; i<10; i++){
+            nomes [i] = leitor.nextLine();
+        }
+        for(int a = 0; a<10; a++){
+            System.out.println("Notas de " + nomes[a]);
+            for(int n=0; n<2; n++){
+                notas [a][n] = leitor.nextDouble();
+            }
+        }
+        double[] mediaAluno = new double [10];
+        double[] mediaAvaliacao = new double [2];
+        double mediaGeral = (mediaAvaliacao[0] + mediaAvaliacao[1])/2.0;
+        
+        
+        System.out.println("Media do aluno: " + mediaAluno[7]);
+        System.out.println("Media Avaliacao: " + mediaAvaliacao[1]);
+        System.out.println("Media geral: " + mediaGeral);
     }
     
 }
