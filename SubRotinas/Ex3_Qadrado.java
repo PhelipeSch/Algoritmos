c58ba1824f9df613eeba70fb116d75abed58f0be
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-07-24 15:08:35 -0300

    Add files via upload

---
+++ SubRotinas/Ex3_Qadrado.java
@@ -0,0 +1,17 @@
+public class Ex3_Qadrado {
+    
+    private double lado;
+
+    public void setlado(double valor){
+        this.lado = valor;
+    }
+    
+    public double getLado(){
+        return this.lado;
+    }
+    
+    public double calcLado(){
+        return lado*lado;
+    }
+    
+}
