53d7f8604b965019629bf60272a9ac11a3c1b2e7
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-08-30 22:46:35 -0300

    Add files via upload

--- Torre_de_Hanoi/Torre_De_Hanoi.java
+++ Torre_de_Hanoi/Torre_De_Hanoi.java
@@ -1,13 +1,45 @@
 import java.util.Scanner;
 public class Torre_De_Hanoi {
+    
+    public static void Movimentos_na_Torre (int discos, String origem, String destino, String apoio){
+        
+        if(discos == 1){
+            System.out.println("Mova o disco mais superficial de "+origem+" para "+destino);
+            return;
+        }
+        
+        Movimentos_na_Torre(discos-1, origem, apoio, destino);
+        System.out.println("Mova o disco mais superficial de "+origem+" para "+destino);
+        Movimentos_na_Torre(discos-1, apoio, destino, origem);
+    }
+    
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         
         System.out.print("Digite o numero de discos da torre: ");
         int discos = leitor.nextInt();
         
+        System.out.println("Digite um nome para o pino de origem: ");
+        String origem = leitor.next();
+        
+        System.out.print("Digite um nome para o pino de destino: ");
+        String destino = leitor.next();
+        
+        System.out.println("Digite um nome para o pino de apoio: ");
+        String apoio = leitor.next();
+      
         double minimo = Math.pow(2, discos)-1;
+        
+        System.out.println("");
         System.out.println("O minimo de movimentos para essa quantidade de discos eh igual a: "+ (int)minimo);
+        
+        System.out.println("");
+        System.out.println("Vamos resolver essa torre com a origem dos discos no pino "+origem+", o pino de apoio sendo "+apoio+" e o destino final sendo o pino "+destino);
+        
+        System.out.println("");
+        System.out.println("Os movimentos devem ser:");
+        
+        Movimentos_na_Torre(discos,origem,destino,apoio);
     }
 }
 /*
