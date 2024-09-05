4e7cfc593d5855be45d8b82c5ce1ee4bf0356dd8
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-09-04 21:13:35 -0300

    Update and rename Exercicio03.java to Exercicio_03.java

--- Arrays/Exercicio_03.java
+++ Arrays/Exercicio_03.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Exercicio03 {
+public class Exercicio_03 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner (System.in);
         //Algoritmo de busca linear de Arrays
