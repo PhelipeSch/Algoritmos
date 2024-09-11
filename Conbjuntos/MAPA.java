cc0ac1e9c52f47e75bd5559a911d3a7cde60383b
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-09-11 15:06:21 -0300

    Add files via upload

---
+++ Conbjuntos/MAPA.java
@@ -0,0 +1,9 @@
+import java.util.Map; // INTERFACE
+import java.util.HashMap; // CLASSE
+import java.util.LinkedHashMap; // CLASSE
+import java.util.TreeMap; // CLASSE
+public class MAPA {
+    public static void main(String[] args) {
+        
+    }
+}
