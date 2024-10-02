c2212a7cce0d8ba970b6092391d6e9169815011f
Phelipe Schmidt <phelipe.schmidt@icloud.com>
2024-10-02 14:15:55 -0300

    Add files via upload

--- Manipulação de Arquivos/EscritaArquivoFormatado.java
+++ Manipulação de Arquivos/EscritaArquivoFormatado.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 public class EscritaArquivoFormatado {
     public static void main(String[] args) {
@@ -32,8 +33,37 @@ public class EscritaArquivoFormatado {
                 leitor.close();
             }
         }
-        for(int i =0; i < codMatricula.size(); i++){
-            System.out.printf("%7d %5.2f %4.1f %02d\n", codMatricula.get(i), notas1.get(i), notas2.get(i), faltas.get(i) );
+        
+        ArrayList< Double > medias = new ArrayList();
+        
+        for(int i = 0; i < notas1.size(); i++){
+            double media = (notas1.get(i) + notas2. get(i) )/2;
+            medias.add(media);
         }
+        
+        File arqSaida = new File("medias.txt");
+        PrintWriter escritor = null;
+        
+        try{
+            escritor = new PrintWriter(arqSaida);
+            
+            for(int i =0; i < codMatricula.size(); i++){
+                
+            escritor.printf("%7d %4.1f %4.1f %2d %4.1f\n", 
+                    codMatricula.get(i), 
+                    notas1.get(i), 
+                    notas2.get(i), 
+                    faltas.get(i),
+                    medias.get(i));
+        }
+        }catch(FileNotFoundException exc){
+            exc.printStackTrace();
+        } finally{
+            if(escritor != null){
+                escritor.close();
+            }
+        }
+        
+        
     }
 }
